618f07bcc6d4bf506f1bb19267f9347d369c6a08
Your Name <you@example.com>
2025-11-16 14:09:23 +0530

    HTTP Methods

--- src/app.js
+++ src/app.js
@@ -2,14 +2,28 @@ const express = require("express");
 
 const App = express();
 
-// App.use((req,res)=>{
-//     res.send("Hello from express server!!!")
-// })
+
+//queryParams
+App.get("/user",(req,res)=>{
+    console.log(req.query)
+    if(req.query.userId==='101')
+    res.send(`Hello user , your query Id is ${req.query.userId}`)
+})
+
+//dynamic routing
+App.get('/user/:userId',(req,res)=>{
+    console.log(req.params)
+    if(req.params.userId==='101')
+    res.send(`Hello user , your param Id is ${req.params.userId}`)
+})
 
 App.use('/hello',(req,res)=>{
     res.send("Hello arrow function")
 })
 
+App.use('/',(req,res)=>{
+    res.send("Hello arrow function")
+})
 App.listen(7779,()=>{
     console.log("Server is listening to port 7779")
 });
